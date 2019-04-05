73ad8182462b3ab74eb4f012761c703b5bd5549c
Rolando Fernandez <rolando@ebi.ac.uk>
2019-04-05 11:04:42 +0100

    added functioality to find and return old versions of given bundles

--- scripts/submission-index/src/app.py
+++ scripts/submission-index/src/app.py
@@ -11,6 +11,7 @@ from functools import reduce
 
 LOGGER = logging.getLogger(__name__)
 
+
 class BundleIndexer:
     def __init__(self, num_threads):
         self.thread_pool = multiprocessing.dummy.Pool(num_threads)
@@ -21,12 +22,14 @@ class BundleIndexer:
         LOGGER.info(f'Found {len(bundle_uuids)} bundles in submission {submission_url}')
 
         start = time.time()
-        bundle_uuids_chunked = list(self.split(bundle_uuids, self.num_threads))
+        bundle_uuids_chunked = list(Util.split(bundle_uuids, self.num_threads))
         enumerated_chunks = list(enumerate(bundle_uuids_chunked))
         now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         results_dirname = f'results_{now}'
         os.mkdir(results_dirname)
-        reindex_results = self.thread_pool.map(lambda enumerated_chunk: BundleIndexer.reindex(enumerated_chunk, results_dirname, Util(dss_api_url)), enumerated_chunks)
+        reindex_results = self.thread_pool.map(
+            lambda enumerated_chunk: BundleIndexer.reindex(enumerated_chunk, results_dirname, Util(dss_api_url)),
+            enumerated_chunks)
         end = time.time()
         print("Time taken: " + str(end - start))
         combined_results = BundleIndexer.reduce_results(reindex_results, results_dirname)
@@ -67,14 +70,9 @@ class BundleIndexer:
         return indexed_bundles_logs
 
     @staticmethod
-    def split(list_to_split, num_chunks):
-        k, m = divmod(len(list_to_split), num_chunks)
-        return (list_to_split[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(num_chunks))
-
-
-    @staticmethod
     def bundle_uuids_for_submission(submission_url):
-        yield from map(lambda bundle_manifest: bundle_manifest["bundleUuid"], BundleIndexer.bundle_manifests_for_submission(submission_url))
+        yield from map(lambda bundle_manifest: bundle_manifest["bundleUuid"],
+                       BundleIndexer.bundle_manifests_for_submission(submission_url))
 
     @staticmethod
     def bundle_manifests_for_submission(submission_url):
@@ -95,9 +93,20 @@ class BundleIndexer:
                     for entity in r.json()["_embedded"][entity_type]:
                         yield entity
 
+class OldBundleCollector:
+    def __init__(self, num_threads):
+        self.thread_pool = multiprocessing.dummy.Pool(num_threads)
+        self.num_threads = num_threads
+
+    def run(self, submission_url, dss_api_url):
+        bundle_uuids = list(BundleIndexer.bundle_uuids_for_submission(submission_url))
+        bundle_uuids_chunks = list(Util.split(bundle_uuids, self.num_threads))
+        old_bundle_fqids = reduce(lambda acc, old_bundle_fqids_chunk: acc + old_bundle_fqids_chunk,
+                                  self.thread_pool.map(lambda bundle_uuids_chunk: Util(dss_api_url).old_bundle_versions(bundle_uuids_chunk),bundle_uuids_chunks),
+                                  [])
+        return old_bundle_fqids
 
 class Util:
-
     class DSSClientCached:
         def __init__(self, dss_api_url):
             self.dss_api_url = dss_api_url
@@ -152,8 +161,34 @@ class Util:
             "old_bundle_version": old_bundle_version
         }
 
+    def old_bundle_versions(self, bundle_uuids):
+        """
+        Given a list of bundle uuids, returns all older versions as list of tuple (<uuid>, <older-version>)
+        :param bundle_uuids:
+        :return:
+        """
+        concat_reducer = lambda acc, old_bundle_versions: acc + old_bundle_versions
+        return reduce(concat_reducer,
+                      map(lambda bundle_uuid: self.old_bundle_version(bundle_uuid), bundle_uuids),
+                      [])
 
-    def create_bundle(self, bundle_uuid, bundle_version, bundle_files, tries = 0, max_tries=5, prev_exc=None):
+    def old_bundle_versions(self, bundle_uuid):
+        """
+        Given a bundle uuid, returns the older versions in a list of tuples (<uuid>, <older-version>)
+        :param bundle_uuid:
+        :return:
+        """
+        bundle_query_with_version = {'query': {'bool': {'must': [{'match': {'uuid': bundle_uuid}}]}}}
+        resp = self.dss_client_cached.get().post_search(replica="aws", es_query=bundle_query_with_version,
+                                                        output_format="raw")
+        results = resp["results"]
+        results_sorted_by_timestamp = sorted(results, key=lambda k: k['bundle_fqid'], reverse=True)
+        if len(results) > 1:
+            return list(map(lambda result: (bundle_uuid, result["metadata"]["manifest"]["version"]), results_sorted_by_timestamp[1:]))
+        else:
+            return []
+
+    def create_bundle(self, bundle_uuid, bundle_version, bundle_files, tries=0, max_tries=5, prev_exc=None):
         if tries >= max_tries:
             raise prev_exc
         else:
@@ -169,7 +204,6 @@ class Util:
                 self.dss_client_cached.refresh_client()
                 time.sleep(1)
                 return self.create_bundle(bundle_uuid, bundle_version, bundle_files, tries + 1, max_tries, e)
-
 
     @staticmethod
     def requires_indexing(bundle_files):
@@ -198,12 +232,25 @@ class Util:
         del _file["size"]
         return _file
 
+    @staticmethod
+    def split(list_to_split, num_chunks):
+        """
+        Given a list and a desired number of chunks, splits the list into num_chunks lists of equal or roughly equal size
+        :param list_to_split:
+        :param num_chunks:
+        :return:
+        """
+        k, m = divmod(len(list_to_split), num_chunks)
+        return (list_to_split[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(num_chunks))
+
 
 if __name__ == "__main__":
     submission_url = sys.argv[1]
     dss_api_url = sys.argv[2]
     num_threads = int(sys.argv[3])
 
-    bundle_indexer = BundleIndexer(num_threads)
-    result_code = bundle_indexer.run(submission_url, dss_api_url)
+    old_fqids = OldBundleCollector(num_threads).run(submission_url, dss_api_url)
+    with open("old_bundle_fqids.json", "rb") as old_fqids_file:
+        json.dump({"results": old_fqids}, old_fqids_file)
+
     sys.exit(result_code)
